4b76f209f323d6fa332457d07adc261a7bf82ad9
Ahmadnur415 <ahmadnursahid12345@gmail.com>
2022-12-09 10:08:11 +0700

    update

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import csv
+import requests
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
+SHEET_ID = "19Y1tZdekS7OOAr6Bii3K_E8wskNudagu1H3wmQ_CzjI"
+SHEET_NAME = "Recipe%20List"
 
 @app.route('/')
 def index():
@@ -12,15 +14,23 @@ def index():
 @app.route('/api/data')
 def data():
     # kamu bisa mendapatkan file csv di
-    # https://docs.google.com/spreadsheets/d/19Y1tZdekS7OOAr6Bii3K_E8wskNudagu1H3wmQ_CzjI/edit#gid=828755875
+    # https://docs.google.com/spreadsheets/d/19Y1tZdekS7OOAr6Bii3K_E8wskNudagu1H3wmQ_CzjI/edit#gid=828755875    
+    URL = f'https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={SHEET_NAME}'
 
-    with open('static/data/data.csv') as csv_file:
+    # write
+    r = requests.get(URL)
+    open('static/data/temp.csv', 'wb').write(r.content)
+
+    # read
+    with open('static/data/temp.csv', 'r') as csv_file:
         df = csv.DictReader(csv_file, delimiter=',')
         data = []
         for row in df:
-            row.pop("S.No.")
-            data.append(row)
+            if row['S.No.']:
+                row.pop("S.No.")
+                data.append(row)
 
+        csv_file.close()
         return {"data": data}
 
 
