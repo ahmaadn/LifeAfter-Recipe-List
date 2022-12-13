b4b6604f6679fed875267de5d22f3441abe1a87b
Ahmadnur415 <ahmadnursahid12345@gmail.com>
2022-12-13 09:10:37 +0700

    command baru `load-data`

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 SHEET_ID = "19Y1tZdekS7OOAr6Bii3K_E8wskNudagu1H3wmQ_CzjI"
 SHEET_NAME = "Recipe%20List"
 
+
 @app.route('/')
 def index():
     return render_template('base.html')
@@ -13,14 +14,6 @@ def index():
 
 @app.route('/api/data')
 def data():
-    # kamu bisa mendapatkan file csv di
-    # https://docs.google.com/spreadsheets/d/19Y1tZdekS7OOAr6Bii3K_E8wskNudagu1H3wmQ_CzjI/edit#gid=828755875    
-    URL = f'https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={SHEET_NAME}'
-
-    # write
-    r = requests.get(URL)
-    open('static/data/temp.csv', 'wb').write(r.content)
-
     # read
     with open('static/data/temp.csv', 'r') as csv_file:
         df = csv.DictReader(csv_file, delimiter=',')
@@ -30,8 +23,18 @@ def data():
                 row.pop("S.No.")
                 data.append(row)
 
-        csv_file.close()
         return {"data": data}
+
+
+@app.cli.command("load-data")
+def load_data():
+    # kamu bisa mendapatkan file csv di
+    # https://docs.google.com/spreadsheets/d/19Y1tZdekS7OOAr6Bii3K_E8wskNudagu1H3wmQ_CzjI/edit#gid=828755875    
+    URL = f'https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={SHEET_NAME}'
+
+    # load data di url
+    r = requests.get(URL)
+    open('static/data/temp.csv', 'wb').write(r.content)
 
 
 if __name__ == '__main__':
